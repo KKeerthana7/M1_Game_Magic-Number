2672a7e9a6a34dba7d9cdff5203e3ca1cff4e858
KKeerthana7 <94303488+KKeerthana7@users.noreply.github.com>
2021-11-24 08:57:43 +0530

    Update main.c

--- 3_Implementation/Src/main.c
+++ 3_Implementation/Src/main.c
@@ -1,1 +1,51 @@
+#iinclude<stdio.h>
 
+
+int sum_of_dig(int x) 
+  
+{
+  
+int s=0, digit;
+  
+while(x>0) 
+  
+{
+digit = x℅10;
+s = s+digit;
+x = x/10;
+  
+  
+}
+return s;
+  
+}
+
+int main()
+  
+{
+int n, sum;
+  
+  
+printf("enter the number\n") ;
+  
+gets("℅d", &n);
+  
+sum = sum_of_dig(n);
+  
+while(sum>0&&sum>9) 
+{
+sum = sum_of_dig(sum);
+  
+}
+  
+if (sum==1) 
+  
+printf("the given number ℅d is Magic Number and sum value is ℅d", n, sum);
+  
+else
+  
+printf ("the given number ℅d is not a Magic Number and sum value is %d", n, sum);
+  
+return 0;
+  
+}
